12d00cf2e165e6434d932f370896e27c8e9bbe21
Staffan <enemark51@hotmail.com>
2021-07-16 14:43:48 +0200

    add text

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/csv-parser'),
+      dir: require('path').join(__dirname, './coverage/xlsx-parser'),
       subdir: '.',
       reporters: [
         { type: 'html' },
